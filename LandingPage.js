21f57f097ec7ccb0cd2d875963883ab4c1da3866
RainbowZephyr <darkfeathers9@gmail.com>
2016-03-31 07:43:52 +0200

    fixing profile form

--- LandingPage.js
+++ LandingPage.js
@@ -62,7 +62,7 @@
       var details;
       event.preventDefault();
       details = {
-        user_id: "1",
+        user_id: "3",
         method: "get_user",
         queue: "USER"
       };
@@ -72,10 +72,18 @@
         datatype: "json",
         data: JSON.stringify(details),
         success: function(result) {
-          var details_form;
-          $("#profile-pane").empty();
-          details_form = "<div class='container'> <div class='row-fluid'> <div class='col-sm-2 col-sm-offset-2'> <img src='" + result.user.avatar_url + "' width='150' height='150'> </div> <div class='col-sm-4'> <form id='profile-form'> <label>Name</label> <input type='text' name='name' value='" + result.user.name + "' size='30' class='form-control'> <br> <label>Username</label> <input type='text' name='username' value='" + result.user.username + "' size='30' class='form-control'> <br> <label>Email</label> <input type='text' name='email' value='" + result.user.email + "' size='30' class='form-control'> <br> <label>Language</label> <input type='text' name='language' value='" + result.user.language + "' size='30' class='form-control'> <br> <label>Country</label> <input type='text' name='country' value='" + result.user.country + "' size='30' class='form-control'> <br> <label>Description</label> <input type='text' name='bio' value='" + result.user.bio + "' size='30' class='form-control'> <br> <label>Website</label> <input type='text' name='website' value='" + result.user.website + "' size='30' class='form-control'> <br> <label>Picture link</label> <input type='text' name='avatar_url' value='" + result.user.avatar_url + "' size='30' class='form-control'> <br> <label>Overlay</label> <input type='text' name='overlay' value='" + result.user.overlay + "' size='30' class='form-control'> <br> <label>Link_color</label> <input type='text' name='link_color' value='" + result.user.link_color + "' size='30' class='form-control'> <br> <label>Background_color</label> <input type='text' name='background_color' value='" + result.user.background_color + "' size='30' class='form-control'> <br> <label>Private Account?</label> <input type='text' name='protected_tweets' value='" + result.user.protected_tweets + "' size='30' class='form-control'> <br> <button type='submit' name='Submit'id='save-profile' class='btn btn-default pull-right'>Save Changes</button> </form> </div> </div> </div>";
-          return $("#profile-pane").append(details_form);
+          $('img[name=profile-image]').prop("src", result.user.avatar_url);
+          $('input[name=name]').val(result.user.name);
+          $('input[name=username]').val(result.user.username);
+          $('input[name=email]').val(result.user.email);
+          $('input[name=language]').val(result.user.language);
+          $('input[name=country]').val(result.user.country);
+          $('input[name=bio]').val(result.user.bio);
+          $('input[name=website]').val(result.user.website);
+          $('input[name=overlay]').val(result.user.overlay);
+          $('input[name=link_color]').val(result.user.link_color);
+          $('input[name=background_color]').val(result.user.background_color);
+          return $('input[name=protected_tweets]').val(result.user.protected_tweets);
         },
         error: function(xhr, status, error) {
           alert("Error");
