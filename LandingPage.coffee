71c9faf2db386a58e81b8a448e8546f3c503b34a
RainbowZephyr <darkfeathers9@gmail.com>
2016-04-10 21:57:17 +0200

    changing layout

--- LandingPage.coffee
+++ LandingPage.coffee
@@ -220,16 +220,14 @@ $(document).ready () ->
             success: (result) ->
                 for i in result.tweets
                     output = "<div class=\"row-fluid\">
-                          <div class=\"col-sm-6 col-sm-offset-3\">
-                            <div class=\"row\">
-                              <div class=\"col-sm-2\"><img class='center-block' src='#{i.creator.avatar_url}' height='50' width='50'></div>
-                              <div class=\"col-sm-10\">
-                                <div class=\"panel panel-default\">
-                                  <div class=\"panel-heading\"> #{capitalize(i.creator.username)} (@#{i.creator.username}) </div>
-                                  <div class=\"panel-body\">#{i.tweet_text}</div>
+                          <div class=\"col-sm-6 col-sm-offset-4\">
+                            <div class=\"media\">
+                              <div class=\"media-left\"><img class='media-object' src='#{i.creator.avatar_url}' height='50' width='50'></div>
+                              <div class=\"media-body\">
+                                  <h4 class=\"media-heading\"> #{capitalize(i.creator.username)} (@#{i.creator.username}) </h4>
+                                  #{i.tweet_text}
                                 </div>
                               </div>
-                            </div>
                           </div>
                         </div>"
                     $("#my-tweets-container").append(output)
@@ -283,7 +281,13 @@ $(document).ready () ->
             data: JSON.stringify(details),
             success: (result) ->
                 console.log result
+                other = ''
                 for i in result.convs
+                    if (i.lastDM.sender.name == localStorage.name)
+                       other = i.lastDM.reciever.name
+                    else
+                       other = i.lastDM.sender.name
+
                     output = "<div class=\"row-fluid\">
                           <div class=\"col-sm-6 col-sm-offset-3\">
                                 <div class=\"media thread\" data-toggle='modal' data-target='.message' id='thread-#{i.id}'>
@@ -291,7 +295,7 @@ $(document).ready () ->
                                     <img class=\"media-object\" src='#{i.lastDM.sender.avatar_url}' alt='DM Image' width='64' height='64'>
                                     </div>
                                     <div class=\"media-body\">
-                                    <h4 class=\"media-heading\">Media heading</h4>
+                                    <h4 class=\"media-heading\">#{other}</h4>
                                     #{i.lastDM.dm_text}
                                     </div>
                                         </div>
@@ -345,10 +349,10 @@ $(document).ready () ->
         event.preventDefault()
         details = {
             user_id: localStorage.user_id,
-            method: "get_convs",
-            queue: "DM"
+            method: "get_subscribed_lists",
+            queue: "USER"
         }
-        console.log localStorage.user_id
+        # console.log localStorage.user_id
 
         $.ajax
             url: "http://localhost:8080",
@@ -357,30 +361,33 @@ $(document).ready () ->
             data: JSON.stringify(details),
             success: (result) ->
                 console.log result
-                for i in result.convs
+                $("#lists-container").empty()
+                for i in result.subscribed_lists
                     output = "<div class=\"row-fluid\">
-                          <div class=\"col-sm-6 col-sm-offset-3\">
-                                <div class=\"media thread\" data-toggle='modal' data-target='.message' id='thread-#{i.id}'>
+                          <div class=\"col-sm-6 col-sm-offset-4\">
+                                <div class=\"media list-entry\" data-toggle='modal' data-target='.list' id='list-#{i.id}'>
                                     <div class=\"media-left\">
-                                    <img class=\"media-object\" src='#{i.lastDM.sender.avatar_url}' alt='DM Image' width='64' height='64'>
+                                    <img class=\"media-object\" src='#{i.creator.avatar_url}' alt='Image' width='64' height='64'>
                                     </div>
                                     <div class=\"media-body\">
-                                    <h4 class=\"media-heading\">Media heading</h4>
-                                    #{i.lastDM.dm_text}
+                                    <h4 class=\"media-heading\">#{capitalize(i.name)} @#{i.creator.username}</h4>
+                                    #{i.description}
                                     </div>
                                         </div>
                         </div>"
 
                     $("#lists-container").append(output)
+
                 $("#lists-container").append("<script>
-                 $('.thread').click(function(event) {
+                 $('.list-entry').click(function(event) {
                    var details, thread_id;
                    event.preventDefault();
-                   thread_id = $(this).attr('id').substring(7);
+                   list_id = $(this).attr('id').substring(5);
+                   console.log(list_id);
                    details = {
-                     conv_id: thread_id,
-                     method: 'get_conv',
-                     queue: 'DM'
+                     list_id: list_id,
+                     method: 'get_list_feeds',
+                     queue: 'LIST'
                    };
                    return $.ajax({
                      url: 'http://localhost:8080',
@@ -390,27 +397,26 @@ $(document).ready () ->
                      success: function(result) {
                        var i, j, len, other, ref, results;
                        console.log(result);
-                       other = '';
-                       if (result.conv.dms[0].sender.name === localStorage.name) {
-                         $('#message-header').empty();
-                         $('#message-header').append(\"<h3>\" + result.conv.dms[0].reciever.name + \"</h3>\");
-                         other = result.conv.dms[0].reciever.name;
-                       } else {
-                         $('#message-header').empty();
-                         $('#message-header').append(\"<h3>\" + result.conv.dms[0].sender.name + \"</h3>\");
-                         other = result.conv.dms[0].sender.name;
-                       }
-                       ref = result.conv.dms;
+                       ref = result.list_feeds;
                        results = [];
-                       $('#message-body').empty();
+                       $('#list-header').empty();
+                       $('#list-body').empty();
+
+                       $('#list-header').append();
                        for (j = 0, len = ref.length; j < len; j++) {
                          i = ref[j];
-                         results.push($('#message-body').append(\"<div class='media'> <div class='media-left'> <a href='#'> <img class='media-object' src='\" + i.sender.avatar_url +\"' alt='Profile' width='42' height='42'> </a> </div> <div class='media-body'> <h4 class='media-heading'>\" + i.sender.name + \"</h4> \" + i.dm_text + \" </div> </div>\"));
+                         results.push($('#list-body').append(\"<div class='media'>
+                          <div class='media-left'>
+                           <a href='#'> <img class='media-object' src='\" + i.creator.avatar_url +\"' alt='Profile' width='64' height='64'> </a>
+                           </div>
+                           <div class='media-body'>
+                            <h4 class='media-heading'>\" + i.creator.name + \"</h4> \" + i.tweet_text + \" </div> </div>\"));
                        }
                        return results;
                      }
                    });
                  });
+                 console.log('finished loop');
                     </script>")
 
 
--- LandingPage.js
+++ LandingPage.js
@@ -225,7 +225,7 @@
           _results = [];
           for (_i = 0, _len = _ref.length; _i < _len; _i++) {
             i = _ref[_i];
-            output = "<div class=\"row-fluid\"> <div class=\"col-sm-6 col-sm-offset-3\"> <div class=\"row\"> <div class=\"col-sm-2\"><img class='center-block' src='" + i.creator.avatar_url + "' height='50' width='50'></div> <div class=\"col-sm-10\"> <div class=\"panel panel-default\"> <div class=\"panel-heading\"> " + (capitalize(i.creator.username)) + " (@" + i.creator.username + ") </div> <div class=\"panel-body\">" + i.tweet_text + "</div> </div> </div> </div> </div> </div>";
+            output = "<div class=\"row-fluid\"> <div class=\"col-sm-6 col-sm-offset-4\"> <div class=\"media\"> <div class=\"media-left\"><img class='media-object' src='" + i.creator.avatar_url + "' height='50' width='50'></div> <div class=\"media-body\"> <h4 class=\"media-heading\"> " + (capitalize(i.creator.username)) + " (@" + i.creator.username + ") </h4> " + i.tweet_text + " </div> </div> </div> </div>";
             _results.push($("#my-tweets-container").append(output));
           }
           return _results;
@@ -280,12 +280,18 @@
         datatype: "json",
         data: JSON.stringify(details),
         success: function(result) {
-          var i, output, _i, _len, _ref;
+          var i, other, output, _i, _len, _ref;
           console.log(result);
+          other = '';
           _ref = result.convs;
           for (_i = 0, _len = _ref.length; _i < _len; _i++) {
             i = _ref[_i];
-            output = "<div class=\"row-fluid\"> <div class=\"col-sm-6 col-sm-offset-3\"> <div class=\"media thread\" data-toggle='modal' data-target='.message' id='thread-" + i.id + "'> <div class=\"media-left\"> <img class=\"media-object\" src='" + i.lastDM.sender.avatar_url + "' alt='DM Image' width='64' height='64'> </div> <div class=\"media-body\"> <h4 class=\"media-heading\">Media heading</h4> " + i.lastDM.dm_text + " </div> </div> </div>";
+            if (i.lastDM.sender.name === localStorage.name) {
+              other = i.lastDM.reciever.name;
+            } else {
+              other = i.lastDM.sender.name;
+            }
+            output = "<div class=\"row-fluid\"> <div class=\"col-sm-6 col-sm-offset-3\"> <div class=\"media thread\" data-toggle='modal' data-target='.message' id='thread-" + i.id + "'> <div class=\"media-left\"> <img class=\"media-object\" src='" + i.lastDM.sender.avatar_url + "' alt='DM Image' width='64' height='64'> </div> <div class=\"media-body\"> <h4 class=\"media-heading\">" + other + "</h4> " + i.lastDM.dm_text + " </div> </div> </div>";
             $("#messages-container").append(output);
           }
           return $("#messages-container").append("<script> $('.thread').click(function(event) { var details, thread_id; event.preventDefault(); thread_id = $(this).attr('id').substring(7); details = { conv_id: thread_id, method: 'get_conv', queue: 'DM' }; return $.ajax({ url: 'http://localhost:8080', type: 'POST', datatype: 'json', data: JSON.stringify(details), success: function(result) { var i, j, len, other, ref, results; console.log(result); other = ''; if (result.conv.dms[0].sender.name === localStorage.name) { $('#message-header').empty(); $('#message-header').append(\"<h3>\" + result.conv.dms[0].reciever.name + \"</h3>\"); other = result.conv.dms[0].reciever.name; } else { $('#message-header').empty(); $('#message-header').append(\"<h3>\" + result.conv.dms[0].sender.name + \"</h3>\"); other = result.conv.dms[0].sender.name; } ref = result.conv.dms; results = []; $('#message-body').empty(); for (j = 0, len = ref.length; j < len; j++) { i = ref[j]; results.push($('#message-body').append(\"<div class='media'> <div class='media-left'> <a href='#'> <img class='media-object' src='\" + i.sender.avatar_url +\"' alt='Profile' width='42' height='42'> </a> </div> <div class='media-body'> <h4 class='media-heading'>\" + i.sender.name + \"</h4> \" + i.dm_text + \" </div> </div>\")); } return results; } }); }); </script>");
@@ -300,10 +306,9 @@
       event.preventDefault();
       details = {
         user_id: localStorage.user_id,
-        method: "get_convs",
-        queue: "DM"
+        method: "get_subscribed_lists",
+        queue: "USER"
       };
-      console.log(localStorage.user_id);
       return $.ajax({
         url: "http://localhost:8080",
         type: "POST",
@@ -312,13 +317,14 @@
         success: function(result) {
           var i, output, _i, _len, _ref;
           console.log(result);
-          _ref = result.convs;
+          $("#lists-container").empty();
+          _ref = result.subscribed_lists;
           for (_i = 0, _len = _ref.length; _i < _len; _i++) {
             i = _ref[_i];
-            output = "<div class=\"row-fluid\"> <div class=\"col-sm-6 col-sm-offset-3\"> <div class=\"media thread\" data-toggle='modal' data-target='.message' id='thread-" + i.id + "'> <div class=\"media-left\"> <img class=\"media-object\" src='" + i.lastDM.sender.avatar_url + "' alt='DM Image' width='64' height='64'> </div> <div class=\"media-body\"> <h4 class=\"media-heading\">Media heading</h4> " + i.lastDM.dm_text + " </div> </div> </div>";
+            output = "<div class=\"row-fluid\"> <div class=\"col-sm-6 col-sm-offset-4\"> <div class=\"media list-entry\" data-toggle='modal' data-target='.list' id='list-" + i.id + "'> <div class=\"media-left\"> <img class=\"media-object\" src='" + i.creator.avatar_url + "' alt='Image' width='64' height='64'> </div> <div class=\"media-body\"> <h4 class=\"media-heading\">" + (capitalize(i.name)) + " @" + i.creator.username + "</h4> " + i.description + " </div> </div> </div>";
             $("#lists-container").append(output);
           }
-          return $("#lists-container").append("<script> $('.thread').click(function(event) { var details, thread_id; event.preventDefault(); thread_id = $(this).attr('id').substring(7); details = { conv_id: thread_id, method: 'get_conv', queue: 'DM' }; return $.ajax({ url: 'http://localhost:8080', type: 'POST', datatype: 'json', data: JSON.stringify(details), success: function(result) { var i, j, len, other, ref, results; console.log(result); other = ''; if (result.conv.dms[0].sender.name === localStorage.name) { $('#message-header').empty(); $('#message-header').append(\"<h3>\" + result.conv.dms[0].reciever.name + \"</h3>\"); other = result.conv.dms[0].reciever.name; } else { $('#message-header').empty(); $('#message-header').append(\"<h3>\" + result.conv.dms[0].sender.name + \"</h3>\"); other = result.conv.dms[0].sender.name; } ref = result.conv.dms; results = []; $('#message-body').empty(); for (j = 0, len = ref.length; j < len; j++) { i = ref[j]; results.push($('#message-body').append(\"<div class='media'> <div class='media-left'> <a href='#'> <img class='media-object' src='\" + i.sender.avatar_url +\"' alt='Profile' width='42' height='42'> </a> </div> <div class='media-body'> <h4 class='media-heading'>\" + i.sender.name + \"</h4> \" + i.dm_text + \" </div> </div>\")); } return results; } }); }); </script>");
+          return $("#lists-container").append("<script> $('.list-entry').click(function(event) { var details, thread_id; event.preventDefault(); list_id = $(this).attr('id').substring(5); console.log(list_id); details = { list_id: list_id, method: 'get_list_feeds', queue: 'LIST' }; return $.ajax({ url: 'http://localhost:8080', type: 'POST', datatype: 'json', data: JSON.stringify(details), success: function(result) { var i, j, len, other, ref, results; console.log(result); ref = result.list_feeds; results = []; $('#list-header').empty(); $('#list-body').empty(); $('#list-header').append(); for (j = 0, len = ref.length; j < len; j++) { i = ref[j]; results.push($('#list-body').append(\"<div class='media'> <div class='media-left'> <a href='#'> <img class='media-object' src='\" + i.creator.avatar_url +\"' alt='Profile' width='64' height='64'> </a> </div> <div class='media-body'> <h4 class='media-heading'>\" + i.creator.name + \"</h4> \" + i.tweet_text + \" </div> </div>\")); } return results; } }); }); console.log('finished loop'); </script>");
         }
       });
     });
